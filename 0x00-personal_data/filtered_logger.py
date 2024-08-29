cfd1467ebe400e5eeee86d29afbfd8cf8299f516
glovic <xtineviktor@gmail.com>
2024-08-30 00:41:01 +0100

    dd get_db function to connect to MySQL database using environment variables

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,3 +69,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
     return logger
 
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Return a MySQL database connection object
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
+    return conn
+
